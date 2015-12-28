d0dbed6a9da263e6cd84022f9131e478d5126459
Emre Avci <eemreavci@gmail.com>
2015-12-28 10:53:22 +0100

    Basic CRUD operations for Posts are working.

--- Server/app/models/post.js
+++ Server/app/models/post.js
@@ -7,4 +7,4 @@ var PostSchema   = new Schema({
     name: String
 });
 
-module.exports = mongoose.model('Post', PostSchema);
+module.exports = db.model('Post', PostSchema);
--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,3 @@
-// server.js
 // https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
 // https://mongolab.com/databases/lifebetdb
 
@@ -11,28 +10,20 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://admin:lifebet1551@ds037095.mongolab.com:37095/lifebetdb'); // connect to our database
+global.db = mongoose.createConnection('mongodb://admin:lifebet1551@ds037095.mongolab.com:37095/lifebetdb'); // connect to the database, global db for creating models on other js files
 
-var db = mongoose.connection;
-db.once('open', function()	{
-	console.log("Connected to database");
-});
-db.on('error', console.error.bind(console, 'Connection error'));
-
+// Get the model
 var Post     = require('./app/models/post');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+// configure app to use bodyParser() to get the data from POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
+// ROUTES FOR THE API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-
+var router = express.Router();
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
@@ -46,16 +37,14 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
-// more routes for our API will happen here
+// Routes for CRUD operations
 router.route('/posts')
 
     // create a post (accessed at POST http://localhost:8080/api/posts)
     .post(function(req, res) {
         
-        var post = new Post();      // create a new instance of the Bear model
-        post.name = req.body.name;  // set the post name (comes from the request)
-
-        post.save();
+        var post = new Post();  
+        post.name = req.body.name;
 
         // save the post and check for errors
         post.save(function(err) {
@@ -67,17 +56,58 @@ router.route('/posts')
 
         
     })
-    // get all the bears (accessed at GET http://localhost:8080/api/bears)
+    // get all the posts (accessed at GET http://localhost:8080/api/posts)
     .get(function(req, res) {
-    	console.log("getting");
         Post.find({}, function(err, posts) {
             if (err)
                 res.send(err);
 
             res.json(posts);
         });
-        console.log("got it");
     });
+
+router.route('/posts/:post_id')
+
+    // get post with id (accessed at GET http://localhost:8080/api/posts/:post_id)
+    .get(function(req, res) {
+        Post.findById(req.params.post_id, function(err, post) {
+            if (err)
+                res.send(err);
+            res.json(post);
+        });
+    })
+
+    // update post with id (accessed at PUT http://localhost:8080/api/posts/:post_id)
+    .put(function(req, res) {
+        Post.findById(req.params.post_id, function(err, post) {
+            if (err)
+                res.send(err);
+
+            post.name = req.body.name; // Update post info
+
+            post.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Post updated!' });
+            });
+
+        });
+    })
+
+    // delete post with id (accessed at DELETE http://localhost:8080/api/posts/:post_id)
+    .delete(function(req, res) {
+        Post.remove({
+            _id: req.params.post_id
+        }, function(err, post) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
